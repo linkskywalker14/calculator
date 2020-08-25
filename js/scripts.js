b918da39c57c79bf0ce56baa7006d51d842492ad
Nick LS Whelan <Nicholas.LS.Whelan@gmail.com>
2020-08-24 20:15:23 -0700

    add additional math functions to the back end which are not yet called by the front end.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,22 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+function remainder(number1, number2){
+  return number1 % number2;
+}
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
